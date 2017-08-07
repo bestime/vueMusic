c7e6b3b7d31acb976ccdf8fc77027d6297b64b51
bestime <1174295440@qq.com>
2017-08-07 17:32:57 +0800

    s

---
+++ src/components/player/player.vue
@@ -0,0 +1,24 @@
+<style scoped>
+    .player{position:fixed;top:0;left:0;width:100%;height:100%;background:#fff;z-index:99;}
+</style>
+
+<template>
+    <div class="player" v-show="playlist.length>0">
+        <div class="normal-player" v-show="fullScreen">
+            播放器
+        </div>
+        <div class="mini-player" v-show="!fullScreen"></div>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex'
+    export default {
+        computed: {
+            ...mapGetters([
+                'fullScreen',
+                'playlist'
+            ])
+        }
+    }
+</script>
